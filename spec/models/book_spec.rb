ac25f008101b9c054c4e46bc661cae683e8422a7
alxjrvs <alxjrvs@gmail.com>
2012-09-22 23:44:12 -0400

    New rspec syntax) :

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 describe Book do
-  before :each do 
+  before :each do
     @book = FactoryGirl.create :book
   end
   it "has a valid factory" do
-    @book.should be_valid
+   expect(@book).to be_valid
   end
   it "is invalid without a name"
   it "is invalid without a descriptioncc"
